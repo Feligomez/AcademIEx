f2b23e587a136a71cadcabde84170868780573f1
GDominguez13 <151012516+GDominguez13@users.noreply.github.com>
2023-11-25 10:15:50 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 
 class Course:
-    def __init__(self, name, subject, duration):
+    def __init__(self, name, subject, weeks, hours):
         self.name = name
         self.subject = subject
-        self.duration = duration
+        self.weeks = weeks
+        self.hours = hours
 
 class Student:
     def __init__(self, name, age, degree, year):
@@ -23,6 +24,13 @@ class Student:
 
     def calculate_mean_goal(self):
         return sum(self.goals.values()) / len(self.goals) if len(self.goals) > 0 else 0
+
+class Schedule:
+    def __init__(self):
+        self.schedule = {day: [] for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]}
+
+    def add_course(self, course, day, hours):
+        self.schedule[day].append((course, hours))
 
 def calculate_priority(student, subject):
     if subject not in student.marks or subject not in student.goals:
@@ -51,7 +59,7 @@ def recommend_courses(student, all_courses, weeks_remaining):
     recommended_courses = {}
 
     for course in all_courses:
-        if course.duration <= weeks_remaining:
+        if course.weeks <= weeks_remaining:
             priority_score, difference = calculate_priority(student, course.subject)
             if priority_score > 0:
                 if course.subject not in recommended_courses:
@@ -80,6 +88,34 @@ def recommend_courses(student, all_courses, weeks_remaining):
                       "courses": [course[0] for course in recommended_courses[subject]["courses"]],
                       "priority": recommended_courses[subject]["priority"],
                       "difference": recommended_courses[subject]["difference"]} for subject in sorted_subjects}
+
+def generate_schedule(student, recommended_courses):
+    available_hours = {}
+    for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]:
+        available_hours[day] = int(input(f"Enter available hours for {day}: "))
+
+    schedule = Schedule()
+
+    for subject in recommended_courses:
+        for course in recommended_courses[subject]["courses"]:
+            day = find_best_day(course, available_hours)
+            hours = min(course.hours, available_hours[day])
+
+            schedule.add_course(course, day, hours)
+            available_hours[day] -= hours
+
+    print("\nGenerated Schedule:")
+    for day in schedule.schedule:
+        if schedule.schedule[day]:
+            print(f"\n{day}:")
+            for course, hours in schedule.schedule[day]:
+                print(f"  {course.name} ({course.subject}) - {hours} hours")
+
+def find_best_day(course, available_hours):
+    # Simple greedy approach: Find the day with the most available hours
+    best_day = max(available_hours, key=available_hours.get)
+    return best_day
+
 
 def get_student_details():
     name = input("Enter student's name: ")
@@ -113,7 +149,7 @@ def display_recommendations(student, weeks_remaining):
 
             print("\nAvailable Courses:")
             for course in sorted_courses:
-                print(f"  {course.name} — (Estimated time for completion: {course.duration} weeks)")
+                print(f"  {course.name} — (Estimated time for completion: {course.weeks} weeks)")
             print("\n————————————————————————————————————")
 
 def display_profile(student):
@@ -154,31 +190,31 @@ def change_marks_goals(student):
     else:
         print(f"You haven't entered marks and goals for {subject} yet.")
 
-# Sample courses (duration in weeks)
+# Courses (duration in weeks, hours per day)
 courses = [
-    Course("QuizletTech1", "Technology", 1),
-    Course("QuizletTech2", "Technology", 1),
-    Course("TechLab1", "Technology", 2),
-    Course("TechLab2", "Technology", 2),
-    Course("Udemy's Flutterflow Course", "Technology", 2),
-    Course("TechProject2", "Technology", 4),
-    Course("YouTubeMath1", "Math", 2),
-    Course("YouTubeMath2", "Math", 2),
-    Course("QuizletMkt1", "Marketing Management", 1),
-    Course("QuizletMkt2", "Marketing Management", 1),
-    Course("BookMkt", "Marketing Management", 2),
-    Course("KhanAcademyMkt2", "Marketing Management", 3),
-    Course("KhanAcademyMkt1", "Marketing Management", 3),
-    Course("KhanAcademyMkt2", "Marketing Management", 3),
-    Course("AdvancedBookMkt", "Marketing Management", 4),
-    Course("AdvancedYouTubeStat", "Statistics", 4),
-    Course("Data Structures & Algorithms Playlist by CS Dojo (Youtube)", "Algorithms & Data Structures", 2),
-    Course("Algorithms and Data Structures Tutorial by freeCodeCamp.org (Youtube)", "Algorithms & Data Structures", 1),
-    Course("Coursera", "Algorithms & Data Structures", 3),
-    Course("Ted Talks", "Building Powerful Relationships", 1),
-    Course("Mind map practice", "Building Powerful Relationships", 1),
-    Course("YouTubeProg1", "Programming for Data Management & Analysis", 3),
-    Course("YouTubeProg2", "Programming for Data Management & Analysis", 3),
+    Course("QuizletTech1", "Technology", 1, 1),
+    Course("QuizletTech2", "Technology", 1, 1),
+    Course("TechLab1", "Technology", 2, 3),
+    Course("TechLab2", "Technology", 2, 3),
+    Course("Udemy's Flutterflow Course", "Technology", 2, 2),
+    Course("TechProject2", "Technology", 4, 4),
+    Course("YouTubeMath1", "Math", 2, 1),
+    Course("YouTubeMath2", "Math", 2, 2),
+    Course("QuizletMkt1", "Marketing Management", 1, 1),
+    Course("QuizletMkt2", "Marketing Management", 1, 1),
+    Course("BookMkt", "Marketing Management", 2, 2),
+    Course("KhanAcademyMkt0", "Marketing Management", 3, 2),
+    Course("KhanAcademyMkt1", "Marketing Management", 3, 2),
+    Course("KhanAcademyMkt2", "Marketing Management", 3, 3),
+    Course("AdvancedBookMkt", "Marketing Management", 4, 2),
+    Course("AdvancedYouTubeStat", "Statistics", 4, 3),
+    Course("Data Structures & Algorithms Playlist by CS Dojo (Youtube)", "Algorithms & Data Structures", 2, 2),
+    Course("Algorithms and Data Structures Tutorial by freeCodeCamp.org (Youtube)", "Algorithms & Data Structures", 1, 2),
+    Course("Coursera", "Algorithms & Data Structures", 3, 2),
+    Course("Ted Talks", "Building Powerful Relationships", 1, 1),
+    Course("Mind map practice", "Building Powerful Relationships", 1, 2),
+    Course("YouTubeProg1", "Programming for Data Management & Analysis", 3, 2),
+    Course("YouTubeProg2", "Programming for Data Management & Analysis", 3, 2),
 ]
 
 # Sample student
@@ -191,23 +227,28 @@ weeks_remaining = int(input("Enter the number of weeks remaining until final exa
 while True:
     print("\nChoose an option:")
     print("1. Recommendations")
-    print("2. Display Profile")
-    print("3. Change Marks and Goals")
-    print("4. Exit")
+    print("2. Generate Schedule")
+    print("3. Display Profile")
+    print("4. Change Marks and Goals")
+    print("5. Exit")
 
-    choice = input("Enter your choice (1, 2, 3, or 4): ")
+    choice = input("Enter your choice (1, 2, 3, 4 or 5): ")
 
     if choice == '1':
         display_recommendations(student1, weeks_remaining)
     elif choice == '2':
-        display_profile(student1)
+        generate_schedule(student1, recommended_courses)
     elif choice == '3':
-        change_marks_goals(student1)
+        display_profile(student1)
     elif choice == '4':
+        change_marks_goals(student1)
+    elif choice == '5':
         print("Exiting the program. Goodbye!")
         break
     else:
-        print("Invalid choice. Please enter 1, 2, 3, or 4.")
+        print("Invalid choice. Please enter 1, 2, 3, 4 or 5.")
+
+
 
 
 
